9d7b77f1e7f44e7634c435a6db063f0dbccc5e6a
NemanjaMandic <nemanjans84@gmail.com>
2019-09-03 21:10:45 +0200

    added classes

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,13 @@
+import React, {SFC} from 'react';
+
+export interface BurgerProps {
+    
+}
+ 
+const Burger: React.SFC<BurgerProps> = () => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default Burger;
